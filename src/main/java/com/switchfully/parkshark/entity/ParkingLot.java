7ccad9c6fe62927c9b7cc52193a892ed408208be
Free Hoebeke <freehoebeke@gmail.com>
2021-11-17 10:41:09 +0100

    Service, Repo and Mapper for PL1 story (by Jordi)

--- src/main/java/com/switchfully/parkshark/entity/ParkingLot.java
+++ src/main/java/com/switchfully/parkshark/entity/ParkingLot.java
@@ -7,7 +7,10 @@ import javax.persistence.*;
 @Entity
 @Table(name = "ParkingLot")
 public class ParkingLot {
+
     @Id
+//    @SequenceGenerator(name = "persons_person_id_seq", sequenceName = "PERSONS_PERSON_id_SEQ", allocationSize = 1)
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "persons_person_id_seq")
     @Column(name = "pl_id")
     private int parkingLotId;
 
