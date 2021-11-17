8275226b3b057d24ead82a8b92d16511a5126ff8
Jordi Maes <jordi19@live.be>
2021-11-17 14:33:40 +0100

    Story PL1 WIP

--- src/main/java/com/switchfully/parkshark/mapper/ParkingLotMapper.java
+++ src/main/java/com/switchfully/parkshark/mapper/ParkingLotMapper.java
@@ -1,18 +1,47 @@
 package com.switchfully.parkshark.mapper;
 
 import com.switchfully.parkshark.dto.CreateParkingLotDTO;
+import com.switchfully.parkshark.dto.ParkingLotDTO;
 import com.switchfully.parkshark.entity.ParkingLot;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ParkingLotMapper {
 
-  /*  public ParkingLot mapCreateParkingLotDTOToParkingLot(CreateParkingLotDTO createParkingLotDTO) {
+    private final AddressMapper addressMapper;
+    private final EmployeeMapper employeeMapper;
+    private final DivisionMapper divisionMapper;
+
+    @Autowired
+    public ParkingLotMapper(AddressMapper addressMapper, EmployeeMapper employeeMapper, DivisionMapper divisionMapper) {
+        this.addressMapper = addressMapper;
+        this.employeeMapper = employeeMapper;
+        this.divisionMapper = divisionMapper;
+    }
+
+   public ParkingLot toEntity(CreateParkingLotDTO createParkingLotDTO) {
 
         return ParkingLot.ParkingLotBuilder.aParkingLot()
                 .withName(createParkingLotDTO.getName())
                 .withMaxCapacity(createParkingLotDTO.getMaxCapacity())
                 .withHourlyPrice(createParkingLotDTO.getHourlyPrice())
-                .with;
-    }*/
+                .withCategory(createParkingLotDTO.getCategory())
+                .withParkingLotAddress(addressMapper.toAddress(createParkingLotDTO.getAddress()))
+                .withEmployee(employeeMapper.toEntity(createParkingLotDTO.getEmployee()))
+                .withDivision(divisionMapper.toEntityDivision(createParkingLotDTO.getDivision()))
+                .build();
+    }
+
+    public ParkingLotDTO toDTO(ParkingLot parkingLot){
+        return ParkingLotDTO.ParkingLotDTOBuilder.aParkingLotDTO()
+                .withName(parkingLot.getName())
+                .withMaxCapacity(parkingLot.getMaxCapacity())
+                .withHourlyPrice(parkingLot.getHourlyPrice())
+                .withCategory(parkingLot.getCategory())
+                .withParkingLotAddress(parkingLot.getParkingLotAddress())
+                .withEmployee(parkingLot.getEmployee())
+                .withDivision(parkingLot.getDivision())
+                .build();
+    }
 }
